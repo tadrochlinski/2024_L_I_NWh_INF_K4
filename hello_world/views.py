f4abae6bd7c676b45c4769d9f07f9ba5070bbb30
tadrochlinski <tadrochlinski@gmail.com>
2024-04-23 20:27:04 +0200

    naprawienie lintera

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,10 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
+
 moje_imie = "Tomasz"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -13,6 +15,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
